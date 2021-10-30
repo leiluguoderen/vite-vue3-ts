5dd3301cac266ffdcdf2ff648717815e753c1069
leiluguoderen <2319743089@qq.com>
2021-10-30 17:42:49 +0800

    ✨ feat(全部): 首次提交

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <ElButton>111</ElButton>
+    <ElButton>111</ElButton>
+    <ElButton>111</ElButton>
+    <ElButton>111</ElButton>
+    <span>HOME</span>
+</template>
+
+<script setup lang="ts">
+import { ElButton } from 'element-plus'
+</script>
+
+<style lang="less">
+.hide{
+    display: none;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path/win32'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  server: {
+    port: 4000, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
+  },
+  plugins: [vue()]
+})
